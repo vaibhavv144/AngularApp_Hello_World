55ade5bcc893c51ddb6b4c116e6a953ca36df94d
Vaibhav <vaibhavsinggh144@gmail.com>
2025-03-21 23:23:14 +0530

    [Vaibhav] Add: Adding UC3 codes

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'HelloWorld';
-  imgUrl = './assets/BL_logo_square_jpg.jpg';
+  imgUrl = 'assets/BL_logo_square_png.png'; // Path to the image
+  url = 'https://www.bridgelabz.com'; // URL to open
 
-  ngOnInit(): void {
-    this.title = "Hello from BridgeLabz.";
+  // Event handler for opening the URL in a new tab
+  onClick(event: Event): void {
+    console.log('BridgeLabz Logo Clicked!', event);
+    window.open(this.url, '_blank'); // Open in a new tab
   }
 }
